85b12b15ff401bb054e352908fb601809179ec37
youkixxshi-tech <youkixxshi@gmail.com>
2026-04-06 04:59:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,19 @@
 import re
-from simpleeval import simple_eval
+import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-# Bot Token ထည့်ရမည့်နေရာ (BotFather ဆီကရတာကို ဒီမှာ ထည့်ပါ)
-TOKEN = "8428492734:AAGI_E83LLQBHaDvpRJw0wWAMCj0aDlrWKM" 
+TOKEN = os.getenv("8428492734:AAGI_E83LLQBHaDvpRJw0wWAMCj0aDlrWKM")
 
 def extract_and_calculate(text):
-    # ဂဏန်းနဲ့ သင်္ချာသင်္ကေတတွေကိုပဲ ရှာတာပါ
     problems = re.findall(r'[0-9+\-*/().\s]+', text)
     results = []
     
     for p in problems:
         p = p.strip()
-        # အနည်းဆုံး 1+1 (စာလုံး ၃ လုံး) ရှိမှ တွက်မယ်
         if len(p) >= 3 and any(c in p for c in "+-*/"):
             try:
-                res = simple_eval(p)
+                res = eval(p)
                 results.append(f"{p} = {res}")
             except:
                 continue
@@ -27,7 +24,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     answer = extract_and_calculate(user_text)
     
     if answer:
-        # ဒီက မြန်မာစာသားတွေက Bot က လူကို ပြန်ဖြေမယ့်စာတွေပါ
         await update.message.reply_text(f"Calculation Result:\n{answer}")
 
 if __name__ == "__main__":
